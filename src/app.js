98daca29dd6b0bcfd8024cbeeab1d28592553c39
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-16 11:14:50 +0530

    created a basic flower catalog page
    by reading file from index.html

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
+const fs = require("fs");
+
 const app = function (req, res) {
-  res.statusCode = 200;
-  console.log("one req came");
-  res.write("hi welcome to my page");
-  res.end();
+  let path = "." + req.url;
+
+  console.log("url is", req.url);
+
+  if(req.url === "/"){
+    path = "." + req.url + "src/index.html";
+  }
+  console.log("path is", path);
+
+  fs.readFile(path, function (error, data) {
+    if (error) {
+      res.statusCode = 404;
+      res.write("invalid request");
+      res.end();
+    } else {
+      console.log("trting to read file");
+      res.write(data);
+      console.log("url after reading", req.url);
+      console.log("path after reading", path);
+      console.log("ending after reading");
+      res.end();
+    }
+  });
 };
 
 // Export a function that can act as a handler
