80f7357382354858445fc5ac9de74b5c80fe1886
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-16 14:35:00 +0530

    Implimented functions sendData and throwError and injected inside fs.readFile

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 const fs = require("fs");
 
+const sendData = function (req, res, data) {
+  res.statusCode = 200;
+  res.write(data);
+  res.end();
+};
+
+const throwError = function (req, res, errorMessage) {
+  res.statusCode = 404;
+  res.write(errorMessage);
+  res.end();
+};
+
 const app = function (req, res) {
-  let path = "." + req.url;
-
-  console.log("url is", req.url);
-
-  if(req.url === "/"){
-    path = "." + req.url + "src/index.html";
+  const errorMessage = "Invalid request";
+  let filePath = "." + req.url;
+  
+  if (req.url === "/") {
+    filePath = "." + req.url + "src/index.html";
   }
-  console.log("path is", path);
 
-  fs.readFile(path, function (error, data) {
-    if (error) {
-      res.statusCode = 404;
-      res.write("invalid request");
-      res.end();
-    } else {
-      console.log("trting to read file");
-      res.write(data);
-      console.log("url after reading", req.url);
-      console.log("path after reading", path);
-      console.log("ending after reading");
-      res.end();
+  fs.readFile(filePath, function (error, data) {
+    if (!error) {
+      sendData(req, res, data);
+      return;
     }
+    throwError(req, res, errorMessage);
   });
 };
 
