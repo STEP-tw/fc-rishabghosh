0ecf9b3685ce55114fd166df219bf148de5264eb
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-02-15 22:25:31 +0530

    Move to express framework, delete old framework

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
-
-const WebFramework = require("./handler.js");
+const express = require("express");
 
 const {
   ERROR_MESSAGE,
@@ -15,31 +14,30 @@ const {
   SPACE
 } = require("./constants.js");
 
-
-const getInitialComments = function () {
+const getInitialComments = function() {
   const initialComments = fs.readFileSync(COMMENT_FILE, UTF8);
   return JSON.parse(initialComments);
 };
 
 let INITIAL_COMMENTS = getInitialComments();
 
-const sendData = function (req, res, data) {
+const sendData = function(req, res, data) {
   res.statusCode = 200;
   res.write(data);
   res.end();
 };
 
-const throwError = function (req, res, errorMessage) {
+const throwError = function(req, res, errorMessage) {
   res.statusCode = 404;
   res.write(errorMessage);
   res.end();
 };
 
-const replacePlusToSpace = function (sourceString) {
+const replacePlusToSpace = function(sourceString) {
   return sourceString.replace(/\+/g, SPACE);
 };
 
-const parser = function (text) {
+const parser = function(text) {
   const eachPair = text.split(AND); //each key-val pair
   const args = {};
   eachPair.forEach(keyValue => {
@@ -55,15 +53,17 @@ const parser = function (text) {
   return args;
 };
 
-const getFilePath = function (url) {
+const getFilePath = function(url) {
   let filePath = "./public" + url;
-  if (url === "/") { filePath = DEFAULT_FILE_PATH; }
+  if (url === "/") {
+    filePath = DEFAULT_FILE_PATH;
+  }
   return filePath;
 };
 
-const serveFiles = function (req, res) {
+const serveFiles = function(req, res) {
   const filePath = getFilePath(req.url);
-  fs.readFile(filePath, function (error, data) {
+  fs.readFile(filePath, function(error, data) {
     if (!error) {
       sendData(req, res, data);
       return;
@@ -72,7 +72,7 @@ const serveFiles = function (req, res) {
   });
 };
 
-const logRequest = function (req, res, next) {
+const logRequest = function(req, res, next) {
   console.log("\n------ LOGS -------\n");
   console.log("requested method ->", req.method);
   console.log("requested url -> ", req.url);
@@ -82,37 +82,43 @@ const logRequest = function (req, res, next) {
   next();
 };
 
-const readBody = function (req, res, next) {
+const readBody = function(req, res, next) {
   let content = EMPTY_STRING;
   res.statusCode = 200;
-  req.on("data", (chunk) => content += chunk);
+  req.on("data", chunk => (content += chunk));
   req.on("end", () => {
     req.body = content;
     next();
   });
 };
 
-const insertTime = function (sourceObject) {
+const insertTime = function(sourceObject) {
   const date = new Date();
   sourceObject["date"] = date;
   return sourceObject;
 };
 
-const generateCommentTable = function (comments) {
-  return comments.map(comment => {
-    const date = new Date(comment.date).toLocaleString();
-    return "<tr>" +
-      `<td>${date}</td>` +
-      `<td>${comment.name}</td>` +
-      `<td>${comment.comment}</td>` +
-      "</tr>";
-  }).join("");
+const generateCommentTable = function(comments) {
+  return comments
+    .map(comment => {
+      const date = new Date(comment.date).toLocaleString();
+      return (
+        "<tr>" +
+        `<td>${date}</td>` +
+        `<td>${comment.name}</td>` +
+        `<td>${comment.comment}</td>` +
+        "</tr>"
+      );
+    })
+    .join("");
 };
 
-
-const renderGuestBook = function (req, res) {
-  fs.readFile(GUEST_BOOK_FILE, function (error, data) {
-    if (error) { throwError(req, res, ERROR_MESSAGE); return; }
+const renderGuestBook = function(req, res) {
+  fs.readFile(GUEST_BOOK_FILE, function(error, data) {
+    if (error) {
+      throwError(req, res, ERROR_MESSAGE);
+      return;
+    }
     const initialHtml = data.toString();
     const table = generateCommentTable(INITIAL_COMMENTS);
     const message = initialHtml.replace(PLACEHOLDER, table);
@@ -120,28 +126,23 @@ const renderGuestBook = function (req, res) {
   });
 };
 
-
-
-const writeNewComment = function (req, res) {
+const writeNewComment = function(req, res) {
   const parsedArgs = parser(req.body);
   const commentWithDate = insertTime(parsedArgs);
 
   INITIAL_COMMENTS.unshift(commentWithDate);
   renderGuestBook(req, res);
   const commentsToWrite = JSON.stringify(INITIAL_COMMENTS);
-  fs.writeFile(COMMENT_FILE, commentsToWrite, (error) => console.error(error));
+  fs.writeFile(COMMENT_FILE, commentsToWrite, error => console.error(error));
 };
 
-const app = function (req, res) {
-  const webFramework = new WebFramework();
-  webFramework.use(readBody);
-  webFramework.use(logRequest);
-  webFramework.get("/guest_book.html", renderGuestBook);
-  webFramework.post("/guest_book.html", writeNewComment);
-  webFramework.use(serveFiles);
-  webFramework.error(throwError);
-  webFramework.handleRequest(req, res);
-};
-
+const app = express();
+app.use(readBody);
+app.use(logRequest);
+app.get("/guest_book.html", renderGuestBook);
+app.post("/guest_book.html", writeNewComment);
+app.use(serveFiles);
+// app.error(throwError);
+// app.handleRequest(req, res);
 
 module.exports = app;
